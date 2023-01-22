db204abb7152e7db4bff3bef32efe142a43d41e7
vicentefiorito <vicentefiorito@gmail.com>
2023-01-21 21:07:29 -0500

    added bg-image and button to redirect to character page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 
@@ -12,27 +13,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       
-      
-
+      <div className={styles.landingContainer}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Genshin Impact Character Index
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A website for information on characters
         </p>
 
-        <button>Character List</button>
-        <button>Weapons</button>
-        <button>Artifacts</button>
-        <button>Enemies</button>
+      <div className={styles.allButton}>
+          <Link href="/characters">Checkout characters!</Link>
+      </div>
 
         <div className={styles.grid}>
           
         </div>
       </main>
+      </div>
+      
 
     </div>
   )
